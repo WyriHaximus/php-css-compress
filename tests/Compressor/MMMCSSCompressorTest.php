2d721ecbb51095042a8be578486302fec55430ad
Cees-Jan Kiewiet <ceesjank@gmail.com>
2022-03-19 22:23:59 +0100

    Drop PHP 7 support

--- tests/Compressor/MMMCSSCompressorTest.php
+++ tests/Compressor/MMMCSSCompressorTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace WyriHaximus\CssCompress\Tests\Compressor;
 
-use WyriHaximus\Compress\AbstractCompressorTest;
 use WyriHaximus\Compress\CompressorInterface;
+use WyriHaximus\Compress\TestUtilities\AbstractCompressorTest;
 use WyriHaximus\CssCompress\Compressor\MMMCSSCompressor;
 
 /**
